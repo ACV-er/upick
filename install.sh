c73b2073924d48e884f05ee4d2f8786aeefe23cd
acver <1246009411@qq.com>
2019-11-20 22:27:36 +0800

    解决权限问题

--- install.sh
+++ install.sh
@@ -3,19 +3,9 @@
 cp .env.example .env
 composer install
 php artisan key:generate
-#项目需要的用户组
-group=www-data
-
-#用户组不存在则创建
-egrep "^$group" /etc/group >& /dev/null
-if [ $? -ne 0 ]
-then
-    groupadd $group
-fi
-
-# 赋予用户组内用户写权限
-sudo chown $USER:www-data -R ./
-sudo chmod g+w -R ./
 
 # 运行docker-compose构建项目,并打开
 docker-compose up --build -d
+
+docker exec -it upick_php chown :www-data -R ./
+docker exec -it upick_php chmod g+w -R ./
