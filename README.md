d246bbfbd82b8716160f104eb097171d16bd358b
acver <1246009411@qq.com>
2019-11-22 17:06:10 +0800

    phpmyadmin配置完毕
    phpmyadmin配置完毕

--- README.md
+++ README.md
@@ -5,13 +5,15 @@
 * 构建条件：  
   *   docker
   *   docker-compose
+  *   npm [非必须，生成文档时使用。不装也必须写好apidoc注释]
 * 构建
   ```
-  sudo chmod a+x install.sh && ./install.sh
+  ./install.sh
   ```
 * web访问端口映射在10303，部署前确保该端口未被占用
 * mysql存在默认密码，做相应修改**\[必须修改\]**
 * laravel.env需要单独配置
+* phpMyAdmin 地址为 `/mathjucry` (数据库)
 
 ## [接口文档](https://git.sky31.com/dinghaodong/UpickBackend/blob/master/api.md)
 > 暂时没有
--- install.sh
+++ install.sh
@@ -3,10 +3,16 @@
 cp .env.example .env
 docker run --rm -it -v $PWD:/app composer:1.9.1 install
 
-# 运行docker-compose构建项目,并打开
+# 删除之前的sql文件
+sudo rm -rf dockercnf/mysql5.7/db_data/*
+
+# 删除之前的容器 运行docker-compose构建项目,并打开
+docker-compose stop
+docker-compose rm
 docker-compose up --build -d
 
 docker exec -it upick_php chown :www-data -R ./
 docker exec -it upick_php chmod g+w -R ./
+sleep 5
 docker exec -it upick_php php artisan key:generate
 docker exec -it upick_php php artisan migrate
