797f7e426636ec8066fde017a6cf8f3bd3fde77d
acver <1246009411@qq.com>
2019-11-23 20:11:22 +0800

    时间配置文件配置 安装脚本优化 nginx配置文件优化

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 
 docker run --rm -it -v $PWD:/app composer:1.9.1 install
 
-# 删除之前的sql文件,上线部署后不执行该步骤
-sudo rm -rf dockercnf/mysql5.7/db_data/*
-
 # 删除之前的容器 运行docker-compose构建项目,并打开
 docker-compose stop
 docker-compose rm
+
+# 删除之前的sql文件,上线部署后不执行该步骤
+sudo rm -rf ./dockercnf/mysql5.7/db_data/*
+
 docker-compose up --build -d
 if [ ! -f ".env" ]; then
   sleep 5
